b202db315068f14ff0bf4aedd2f36808c6a41800
haq <haqgamer66@gmail.com>
2020-01-12 22:53:41 -0500

    updated login success route

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,4 @@
 <?php
 
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
-
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
