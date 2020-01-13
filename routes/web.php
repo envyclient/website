7295fb1213fd8e300418a766316b5a3d38038d7c
haq <haqgamer66@gmail.com>
2020-01-12 23:18:48 -0500

    added paypal payments boiler code

--- routes/web.php
+++ routes/web.php
@@ -2,3 +2,8 @@
 
 Auth::routes();
 Route::get('/', 'HomeController@index')->name('home');
+
+
+// TODO: change routes
+Route::get('paypal/ec-checkout', 'PayPalController@getExpressCheckout');
+Route::get('paypal/ec-checkout-success', 'PayPalController@getExpressCheckoutSuccess');
