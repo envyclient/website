9b342045202b480a8895af1659c04df47d24490d
haq <haqgamer66@gmail.com>
2020-05-12 17:38:39 -0400

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -24,7 +24,9 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-
+        $schedule->call(function () {
+            echo 'called';
+        })->everyMinute();
     }
 
     /**
