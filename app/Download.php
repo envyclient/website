48b7dacd971e161aadf93824fb096fa8ec365327
haq <haqgamer66@gmail.com>
2020-05-14 20:22:29 -0400

    updated todo

--- app/Download.php
+++ app/Download.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
  * @property  string name
  * @property  string file
  */
-// TODO: rename to version
+// TODO: rename to version, ability to remove version
 class Download extends Model
 {
     const FILES_DIRECTORY = 'downloads';
