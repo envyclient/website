8c823f2988be1bced0bb79762d6a0db2ff5e080f
haq <haqgamer66@gmail.com>
2020-05-23 00:03:28 -0400

    updated terms

--- resources/views/pages/terms.blade.php
+++ resources/views/pages/terms.blade.php
@@ -4,53 +4,78 @@
     <div class="container mt-5">
         @markdown
         # Terms of Service
-        This license is a legal agreement between you and Envy Client ("we") for the use of Envy Client (the
+        This license is a legal agreement between you and Haq Development (“we”) for the use of Envy Client (the
         “Software”). By downloading any Envy Client files you agree to be bound by the terms and conditions of this
-        license. We reserve the right to alter this agreement at any time, for any reason, without notice.
+        license. We reserve the right to alter this agreement at any time, for any reason, without notice. By continuing
+        to use Envy Client, you will be deemed to have accepted any changes.
 
         ## Permitted Use
-        Each account is tied to a physical machine. Each additional physical machine using the Software requires an
+        * Each account is tied to a physical machine. Each additional physical machine using the software requires an
         additional account with an active subscription.
+        * Envy Client may only be accessed on Microsoft Windows.
+
+        ## Rights of Haq Development
+        * Haq Development reserves the right to modify or terminate Envy Client accounts at any time, for any reason.
+        * Haq Development reserves the right to modify or terminate Envy Client services at any time, for any reason.
 
         ## Restrictions
-        You may:
-        * Use the Software only on Microsoft Windows. Linux and macOS are not supported.
-
-        You may not:
-        * Reproduce, distribute, or transfer the Software, or portions thereof, to any third party.
-        * Sell, rent, lease, assign and modify the Software or portions thereof.
+        It is not permissible to:
+        * Reproduce, distribute, or transfer Envy Client, or portions thereof, to any third party.
+        * Sell, rent, lease, assign, and modify Envy Client or portions thereof.
         * Grant rights to any other person.
-
         Any violations of these restrictions are grounds for termination.
 
+        ## Payment Practices
+        Haq Development will use PayPal to receive payments.
+        * The subscription for Envy Client will be in thirty-day billing cycles.
+        * Renewals will occur automatically, unless cancelled.
+        * Haq Development reserves the right to change prices at any time, for any reason.
+        * Haq Development also reserves the right to suspend service with sole discretion, even if the service would be
+        paid for due to violations of the Terms of Service.
+        * Should payment not be received for an account with an active monthly billing cycle, then that account will
+        lose Envy Client service access.
+        * All payments will be made in US Dollars.
+
         ## Refunds
-        We offer refunds provided you contact us with 24 hours of purchase, with a valid reason. We do not offer trials.
-        After these 24 hours, you are no longer eligible for a refund.
+        Requested a refund within 24 days of purchasing a subscription to the Software.
+        * Not have used downloaded the Software after purchasing a subscription.
+        * Not have your account banned.
+        * Not have opened a chargeback request on PayPal.
 
         ## Data Collection
-        We try to collect very minimal data but here is what we collect:
-        * Hardware Identification that can and is used to identify a user's machine.
-        * Telemetry data consisting of how the Software is used.
+        Haq Development strives to collect as very little data as possible. However, the following are collected:
+        * Hardware Identification information that can and is used to identify a user’s machine.
+        * Telemetry data consisting of how Envy Client is used.
+        * Personal information used to create your account for Envy Client.
 
-        ## Cookies
-        https://panel.envyclient.com relies on "cookie" technology to allow you to re-access your registered account
-        profile during a web session. This mechanism is employed solely for your convenience and no other purpose.
+        Should there be any issue regarding data collection, suspension, or termination of accounts, contact
+        <contact
+        @envyclient.com>.
+        Should there be any information you determine to be your right according to Canada’s Personal Information
+        Protection and Electronic Documents Act (PIPEDA) or the European Union’s General Data Protection Regulation
+        (GDPR), contact us to sort out any inquiries.
 
-        ## Contact
-        Questions about the Terms of Service should be sent to us at
-        [contact@envyclient.com](mailto:contact@envyclient.com).
+        All data collection practices are pursuant to Canadian and EU regulations.
 
-        ## Disclaimer Of Warranty
+        ## Disclaimer of Warranty
         THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED
         TO, WARRANTIES OF QUALITY, PERFORMANCE, NON-INFRINGEMENT, MERCHANTABILITY, OR FITNESS FOR A PARTICULAR PURPOSE.
         FURTHER, ENVY CLIENT DOES NOT WARRANT THAT THE SOFTWARE OR ANY RELATED SERVICE WILL ALWAYS BE AVAILABLE.
 
-        ## Limitations Of Liability
+        ## Limitations of Liability
         YOU ASSUME ALL RISK ASSOCIATED WITH THE INSTALLATION AND USE OF THE SOFTWARE. IN NO EVENT SHALL THE AUTHORS OR
         COPYRIGHT HOLDERS OF THE SOFTWARE BE LIABLE FOR CLAIMS, DAMAGES OR OTHER LIABILITY ARISING FROM, OUT OF, OR IN
         CONNECTION WITH THE SOFTWARE. LICENSE HOLDERS ARE SOLELY RESPONSIBLE FOR DETERMINING THE APPROPRIATENESS OF USE
         AND ASSUME ALL RISKS ASSOCIATED WITH ITS USE, INCLUDING BUT NOT LIMITED TO THE RISKS OF PROGRAM ERRORS, DAMAGE
         TO EQUIPMENT, LOSS OF DATA OR SOFTWARE PROGRAMS, OR UNAVAILABILITY OR INTERRUPTION OF OPERATIONS.
+
+        ## Contact Us and Dispute Resolution
+        If the above does not pertain to your inquiries, contact us at
+        <contact
+        @envyclient.com>.
+
+        Matter of dispute resolution may also be handled by email or even phone correspondence should something
+        escalate.
         @endmarkdown
     </div>
 @endsection
