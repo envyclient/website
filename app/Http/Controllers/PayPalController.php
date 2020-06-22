d89ba257b92f3316df5ac6b02c43cf7921060f9e
Affan Haq <haqgamer66@gmail.com>
2020-06-22 14:54:31 -0400

    started to add paypal subscriptions

--- app/Http/Controllers/PayPalController.php
+++ app/Http/Controllers/PayPalController.php
@@ -2,20 +2,21 @@
 
 namespace App\Http\Controllers;
 
-use App\Invoice;
-use App\Notifications\Generic;
 use App\Providers\RouteServiceProvider;
+use Carbon\Carbon;
 use Exception;
 use Illuminate\Http\Request;
-use PayPal\Api\Amount;
-use PayPal\Api\Item;
-use PayPal\Api\ItemList;
+use PayPal\Api\Agreement;
+use PayPal\Api\Currency;
+use PayPal\Api\MerchantPreferences;
+use PayPal\Api\Patch;
+use PayPal\Api\PatchRequest;
 use PayPal\Api\Payer;
-use PayPal\Api\Payment;
-use PayPal\Api\PaymentExecution;
-use PayPal\Api\RedirectUrls;
-use PayPal\Api\Transaction;
+use PayPal\Api\PaymentDefinition;
+use PayPal\Api\Plan;
 use PayPal\Auth\OAuthTokenCredential;
+use PayPal\Common\PayPalModel;
+use PayPal\Exception\PayPalConnectionException;
 use PayPal\Rest\ApiContext;
 
 class PayPalController extends Controller
@@ -33,9 +34,129 @@ class PayPalController extends Controller
         $this->paypal->setConfig(config('paypal.settings'));
     }
 
+    public function createBillingPlan()
+    {
+        // Create a new billing plan
+        $plan = new Plan();
+        $plan->setName('Premium')
+            ->setDescription('1 month of recurring Envy Client')
+            ->setType('INFINITE');
+
+        // Set billing plan definitions
+        $paymentDefinition = new PaymentDefinition();
+        $paymentDefinition->setName('Regular Payments')
+            ->setType('REGULAR')
+            ->setFrequency('Month')
+            ->setFrequencyInterval('1')
+            //->setCycles('12')
+            ->setAmount(new Currency([
+                'value' => 10,
+                'currency' => 'USD'
+            ]));
+
+        // Set merchant preferences
+        $merchantPreferences = new MerchantPreferences();
+        $merchantPreferences->setReturnUrl(route('paypal.executeBillingAgreement'))
+            ->setCancelUrl(route('paypal.cancel'))
+            ->setAutoBillAmount('YES')
+            ->setInitialFailAmountAction('CANCEL')
+            ->setMaxFailAttempts('0')
+            ->setSetupFee(new Currency([
+                'value' => 10,
+                'currency' => 'USD'
+            ]));
+
+        $plan->setPaymentDefinitions(array($paymentDefinition));
+        $plan->setMerchantPreferences($merchantPreferences);
+
+        try {
+            $createdPlan = $plan->create($this->paypal);
+
+            try {
+                $patch = new Patch();
+                $value = new PayPalModel('{"state":"ACTIVE"}');
+                $patch->setOp('replace')
+                    ->setPath('/')
+                    ->setValue($value);
+                $patchRequest = new PatchRequest();
+                $patchRequest->addPatch($patch);
+                $createdPlan->update($patchRequest, $this->paypal);
+                $plan = Plan::get($createdPlan->getId(), $this->paypal);
+
+                dd($plan);
+
+            } catch (PayPalConnectionException $ex) {
+                echo $ex->getCode();
+                echo $ex->getData();
+                die($ex);
+            } catch (Exception $ex) {
+                die($ex);
+            }
+        } catch (PayPalConnectionException $ex) {
+            echo $ex->getCode();
+            echo $ex->getData();
+            die($ex);
+        } catch (Exception $ex) {
+            die($ex);
+        }
+    }
+
+    public function processAgreement()
+    {
+        // Create new agreement
+        $agreement = new Agreement();
+        $agreement->setName('Base Agreement')
+            ->setDescription('Basic Agreement')
+            ->setStartDate(Carbon::now()->addMinutes(1)->toIso8601String());
+
+        // Set plan id
+        $plan = new Plan();
+        $plan->setId('P-2XF851689H528853W3UZLKOA');
+        $agreement->setPlan($plan);
+
+        // Add payer type
+        $payer = new Payer();
+        $payer->setPaymentMethod('paypal');
+        $agreement->setPayer($payer);
+
+        try {
+            // Create agreement
+            $agreement = $agreement->create($this->paypal);
+
+            return redirect()->away($agreement->getApprovalLink());
+        } catch (Exception $ex) {
+            dd($ex);
+        }
+    }
+
+    public function executeBillingAgreement(Request $request)
+    {
+        if (!$request->has('token')) {
+            return redirect(RouteServiceProvider::HOME)->with('error', 'Payment failed.');
+        }
+
+        $token = $request->token;
+        $agreement = new Agreement();
+
+        try {
+            // Execute agreement
+            $result = $agreement->execute($token, $this->paypal);
+
+            dd($result);
+
+        } catch (PayPalConnectionException $ex) {
+            echo $ex->getCode();
+            echo $ex->getData();
+            die($ex);
+        } catch (Exception $ex) {
+            die($ex);
+        }
+
+    }
+
     public function create(Request $request)
     {
-        $this->validate($request, [
+        /*$this->validate($request, [
             'amount' => 'required|integer|between:5,100'
         ]);
 
@@ -84,12 +205,12 @@ class PayPalController extends Controller
             'payment_id' => $payment->getId(),
             'price' => $price
         ]);
-        return redirect()->away($payment->getApprovalLink());
+        return redirect()->away($payment->getApprovalLink());*/
     }
 
     public function success(Request $request)
     {
-        $paymentID = session('payment_id');
+        /*$paymentID = session('payment_id');
         $price = session('price');
         session()->forget(['payment_id', 'price']);
 
@@ -128,12 +249,12 @@ class PayPalController extends Controller
             return redirect(RouteServiceProvider::HOME)->with('success', 'Payment success.');
         }
 
-        return redirect(RouteServiceProvider::HOME)->with('error', 'Payment failed.');
+        return redirect(RouteServiceProvider::HOME)->with('error', 'Payment failed.');*/
     }
 
     public function cancel()
     {
-        return redirect(RouteServiceProvider::HOME)->with('error', 'Payment cancelled.');
+        return redirect(RouteServiceProvider::HOME)->with('error', 'Subscription cancelled.');
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,10 @@ Route::prefix('subscriptions')->group(function () {
  * Payments
  */
 Route::prefix('paypal')->group(function () {
+    Route::get('createBillingPlan', 'PayPalController@createBillingPlan')->name('paypal.createBillingPlan');
+    Route::get('processAgreement', 'PayPalController@processAgreement')->name('paypal.processAgreement');
+    Route::get('executeBillingAgreement', 'PayPalController@executeBillingAgreement')->name('paypal.executeBillingAgreement');
+
     Route::post('create', 'PayPalController@create')->name('paypal.create');
     Route::get('success', 'PayPalController@success')->name('paypal.success');
     Route::get('cancel', 'PayPalController@cancel')->name('paypal.cancel');
