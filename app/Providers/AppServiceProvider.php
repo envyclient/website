fc6f1682240e703af04e4754b25a789d4938668c
Affan Haq <haqgamer66@gmail.com>
2020-10-11 12:25:34 -0400

    initial commit

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use App\Observers\UserObserver;
 use App\User;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -30,5 +31,6 @@ class AppServiceProvider extends ServiceProvider
         Schema::defaultStringLength(191);
         JsonResource::withoutWrapping();
         User::observe(UserObserver::class);
+        Paginator::useBootstrap();
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,51 +1,57 @@
 <?php
 
+use App\Http\Controllers\API\AdminController;
+use App\Http\Controllers\API\AuthController;
+use App\Http\Controllers\API\ChartsController;
+use App\Http\Controllers\API\ConfigsController;
+use App\Http\Controllers\API\GameSessionsController;
+use App\Http\Controllers\API\HandlePayPalWebhook;
+use App\Http\Controllers\API\VersionsController;
+use App\Http\Controllers\DownloadAssets;
+use Illuminate\Support\Facades\Route;
+
 /**
  * Auth
  */
-
-use Illuminate\Support\Facades\Route;
-
 Route::prefix('auth')->group(function () {
-    Route::post('login', 'API\AuthController@login');
-    Route::get('me', 'API\AuthController@me');
+    Route::post('login', [AuthController::class, 'login']);
+    Route::get('me', [AuthController::class, 'me']);
 });
 
 /**
  * Configs
  */
 Route::prefix('configs')->group(function () {
-    Route::put('favorite/{config}', 'API\ConfigsController@favorite');
+    Route::put('favorite/{config}', [ConfigsController::class, 'favorite']);
 
-    Route::get('user', 'API\ConfigsController@getCurrentUserConfigs');
-    Route::get('user/{name}', 'API\ConfigsController@getConfigsByUser');
+    Route::get('user', [ConfigsController::class, 'getCurrentUserConfigs']);
+    Route::get('user/{name}', [ConfigsController::class, 'getConfigsByUser']);
 
-    Route::get('/', 'API\ConfigsController@index');
-    Route::get('{config}', 'API\ConfigsController@show');
-    Route::post('/', 'API\ConfigsController@store');
-    Route::put('{config}', 'API\ConfigsController@update');
-    Route::delete('{config}', 'API\ConfigsController@destroy');
+    Route::get('/', [ConfigsController::class, 'index']);
+    Route::get('{config}', [ConfigsController::class, 'show']);
+    Route::post('/', [ConfigsController::class, 'store']);
+    Route::put('{config}', [ConfigsController::class, 'update']);
+    Route::delete('{config}', [ConfigsController::class, 'destroy']);
 });
 
 /**
  * Admin
  */
 Route::prefix('admin')->group(function () {
-    Route::get('transactions', 'API\AdminController@transactions')->name('api.admin.transactions');
-    Route::get('users', 'API\AdminController@users')->name('api.admin.users');
-    Route::put('users/ban/{user}', 'API\AdminController@ban')->name('api.admin.users.ban');
+    Route::get('users', [AdminController::class, 'users'])->name('api.admin.users');
+    Route::put('users/ban/{user}', [AdminController::class, 'ban'])->name('api.admin.users.ban');
 });
 
 /**
  * Charts
  */
 Route::prefix('charts')->group(function () {
-    Route::get('users', 'API\ChartsController@users')->name('api.charts.users');
-    Route::get('versions', 'API\ChartsController@versions')->name('api.charts.versions');
+    Route::get('users', [ChartsController::class, 'users'])->name('api.charts.users');
+    Route::get('versions', [ChartsController::class, 'versions'])->name('api.charts.versions');
 
     Route::prefix('sessions')->group(function () {
-        Route::get('onTime', 'API\ChartsController@ontime')->name('api.charts.sessions.ontime');
-        Route::get('toggles', 'API\ChartsController@toggles')->name('api.charts.sessions.toggles');
+        Route::get('onTime', [ChartsController::class, 'onTime'])->name('api.charts.sessions.ontime');
+        Route::get('toggles', [ChartsController::class, 'toggles'])->name('api.charts.sessions.toggles');
     });
 });
 
@@ -53,21 +59,21 @@ Route::prefix('charts')->group(function () {
  * Versions
  */
 Route::prefix('versions')->group(function () {
-    Route::get('/', 'API\VersionsController@index')->name('api.versions.index');
-    Route::get('{version}', 'API\VersionsController@show')->name('api.versions.show');
-    Route::delete('{version}', 'API\VersionsController@destroy')->name('api.versions.delete');
+    Route::get('/', [VersionsController::class, 'index'])->name('api.versions.index');
+    Route::get('{version}', [VersionsController::class, 'show'])->name('api.versions.show');
+    Route::delete('{version}', [VersionsController::class, 'destroy'])->name('api.versions.delete');
 });
 
 /**
  * Game Sessions
  */
 Route::prefix('sessions')->group(function () {
-    Route::post('/', 'API\GameSessionsController@store');
-    Route::put('{session}', 'API\GameSessionsController@update');
+    Route::post('/', [GameSessionsController::class, 'store']);
+    Route::put('{session}', [GameSessionsController::class, 'update']);
 });
 
 /**
  * Extra
  */
-Route::get('assets', 'API\VersionsController@assets');
-Route::post('paypal', 'API\HandlePayPalWebhook');
+Route::get('assets', DownloadAssets::class);
+Route::post('paypal', HandlePayPalWebhook::class);
