b42c0c3fde1da0df5bfb9f93314cfd84cfd3ab63
haaq <haqgamer66@gmail.com>
2020-10-16 21:17:41 -0400

    updated new subscription email

--- resources/views/emails/subscription.blade.php
+++ resources/views/emails/subscription.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 # New Subscription
 
-Thank you for subscribing to a plan {{ $user->name }}!
+Thank you for subscribing to the {{ $user->subscription->plan->name }} plan!
 
 @component('mail::table')
 | Plan | Interval | Price |
@@ -9,8 +9,8 @@ Thank you for subscribing to a plan {{ $user->name }}!
 | {{ $user->subscription->plan->name }} | 30 days | {{ $user->subscription->plan->price }} |
 @endcomponent
 
-@component('mail::button', ['url' => url('/')])
-Visit Website
+@component('mail::button', ['url' => url('subscriptions')])
+Manage Subscription
 @endcomponent
 
 Thanks,<br>
