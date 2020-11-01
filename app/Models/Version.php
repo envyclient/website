1608a027f32070e4d248752b435818e2cb6ece7e
Affan Haq <haqgamer66@gmail.com>
2020-11-01 15:53:22 -0500

    updated composer packages

--- app/Models/Version.php
+++ app/Models/Version.php
@@ -5,13 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * @property  string name
- * @property  boolean beta
- * @property  string file
+ * @property string name
+ * @property boolean beta
+ * @property string file
  */
 class Version extends Model
 {
     const FILES_DIRECTORY = 'versions';
+
+    protected $fillable = [
+        'name',
+        'beta',
+        'file',
+    ];
 
     public function users()
     {
