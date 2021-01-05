632d788458fb8ea153926be14598669416c85b10
Affan Haq <haqgamer66@gmail.com>
2021-01-04 20:40:54 -0500

    added cookie based referral system

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -2,7 +2,9 @@
 
 namespace App\Observers;
 
+use App\Models\ReferralCode;
 use App\Models\User;
+use Illuminate\Support\Facades\Cookie;
 
 class UserObserver
 {
@@ -11,5 +13,15 @@ class UserObserver
         $user->api_token = bin2hex(openssl_random_pseudo_bytes(30));
         $user->image = 'https://avatar.tobi.sh/avatar/' . md5(strtolower(trim($user->email))) . '.svg?text=' . strtoupper(substr($user->name, 0, 2));
         $user->cape = asset('assets/capes/default.png');
+
+        // handle referral code cookie
+        if (request()->hasCookie('referral') && ReferralCode::where('code', request()->cookie('referral'))->exists()) {
+            $code = ReferralCode::where('code', request()->cookie('referral'))->first();
+            $user->referral_code_id = $code->id;
+            $user->referral_code_used_at = now();
+
+            // forgetting the referral cookie
+            Cookie::queue(Cookie::forget('referral'));
+        }
     }
 }
