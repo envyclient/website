159598d805008f67f0607cfd160960902ee31e74
Affan Haq <haqgamer66@gmail.com>
2021-01-12 11:14:34 -0500

    Update UsersTable.php

--- app/Http/Livewire/UsersTable.php
+++ app/Http/Livewire/UsersTable.php
@@ -80,15 +80,14 @@ class UsersTable extends Component
 
         // removing the users subscription to the free plan
         if ($removing) {
-            // TODO: remove subscription
-            /*  Subscription::where('user_id', $id)
-                  ->where('plan_id', 1)
-                  ->delete();*/
+            Subscription::where('user_id', $id)
+                ->where('plan_id', 1)
+                ->delete();
         } else {
             // give the user premium plan
             Subscription::create([
                 'user_id' => $user->id,
-                'plan_id' => 3,
+                'plan_id' => 1,
                 'end_date' => now()->addMonth(),
             ]);
         }
