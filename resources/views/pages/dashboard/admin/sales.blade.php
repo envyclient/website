6bfe696a5b89e25f92ba4eec616739d4be16624b
Affan Haq <haqgamer66@gmail.com>
2021-01-16 20:52:06 -0500

    wip

--- resources/views/pages/dashboard/admin/sales.blade.php
+++ resources/views/pages/dashboard/admin/sales.blade.php
@@ -10,22 +10,21 @@
             <div class="col-md-3" style="display:inline-block;margin:0 auto;">
                 <div class="card text-center" style="padding:10px;">
                     <i class="fas fa-coins fa-2x"></i>
-                    <h2> {{ 99.99 }}</h2>
+                    <h2>$xx.xx</h2>
                     <h4>Total</h4>
                 </div>
             </div>
             <div class="col-md-3" style="display:inline-block;margin:0 auto;">
                 <div class="card text-center" style="padding:10px;">
                     <i class="fas fa-coins fa-2x"></i>
-                    <h2>{{ 99.99 }}</h2>
+                    <h2>$xx.xx</h2>
                     <h4>Standard</h4>
                 </div>
             </div>
             <div class="col-md-3" style="display:inline-block;margin:0 auto;">
                 <div class="card text-center" style="padding:10px;">
                     <i class="fas fa-coins fa-2x"></i>
-                    <h2>{{ 99.99 }}
-                    </h2>
+                    <h2>$xx.xx</h2>
                     <h4>Premium</h4>
                 </div>
             </div>
