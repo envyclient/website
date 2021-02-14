644d5e51781e7e97aecd1a6f7a1f1c5420fbd08a
Affan Haq <haqgamer66@gmail.com>
2021-02-14 11:37:31 -0500

    Update plan-card.blade.php

--- resources/views/components/plan-card.blade.php
+++ resources/views/components/plan-card.blade.php
@@ -8,7 +8,7 @@
         </h6>
 
         <div class="mb-2">
-            <svg style="width:20px;height:20px" viewBox="0 0 20 20" fill="green">
+            <svg width="20" height="20" viewBox="0 0 20 20" fill="green">
                 <path fill-rule="evenodd"
                       d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
                       clip-rule="evenodd"/>
@@ -17,7 +17,7 @@
         </div>
 
         <div class="mb-2">
-            <svg style="width:20px;height:20px" viewBox="0 0 20 20" fill="green">
+            <svg width="20" height="20" viewBox="0 0 20 20" fill="green">
                 <path fill-rule="evenodd"
                       d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
                       clip-rule="evenodd"/>
@@ -26,7 +26,7 @@
         </div>
 
         <div class="mb-2">
-            <svg style="width:20px;height:20px" viewBox="0 0 20 20" fill="green">
+            <svg width="20" height="20" viewBox="0 0 20 20" fill="green">
                 <path fill-rule="evenodd"
                       d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
                       clip-rule="evenodd"/>
@@ -36,7 +36,7 @@
 
         @if($plan->beta_access)
             <div class="mb-2">
-                <svg style="width:20px;height:20px" viewBox="0 0 20 20" fill="green">
+                <svg width="20" height="20" viewBox="0 0 20 20" fill="green">
                     <path fill-rule="evenodd"
                           d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
                           clip-rule="evenodd"/>
@@ -47,7 +47,7 @@
 
         @if($plan->capes_access)
             <div class="mb-2">
-                <svg style="width:20px;height:20px" viewBox="0 0 20 20" fill="green">
+                <svg width="20" height="20" viewBox="0 0 20 20" fill="green">
                     <path fill-rule="evenodd"
                           d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z"
                           clip-rule="evenodd"/>
@@ -57,9 +57,10 @@
         @endif
 
         <div>
-            <svg style="width:20px;height:20px" viewBox="0 0 24 24">
-                <path fill="currentColor"
-                      d="M3,12V6.75L9,5.43V11.91L3,12M20,3V11.75L10,11.9V5.21L20,3M3,13L9,13.09V19.9L3,18.75V13M20,13.25V22L10,20.09V13.1L20,13.25Z"/>
+            <svg style="width:20px;height:20px" viewBox="0 0 20 20" fill="#eb5e0b">
+                <path fill-rule="evenodd"
+                      d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z"
+                      clip-rule="evenodd"/>
             </svg>
             <span class="ms-1">Windows Only</span>
         </div>
