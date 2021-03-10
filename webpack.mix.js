2e04dbf06fca483599ad65ee5192a485a61083ea
Affan Haq <haqgamer66@gmail.com>
2021-03-09 19:21:19 -0500

    added purgecss

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,7 @@
 const mix = require('laravel-mix');
+require('laravel-mix-purgecss');
 
 mix.js('resources/js/app.js', 'public/js')
     .css('resources/css/app.css', 'public/css')
+    .purgeCss()
     .disableNotifications();
