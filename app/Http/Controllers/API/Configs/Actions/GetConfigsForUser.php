e4d1c19cb3acb2c8e9f8bbb298431e1fca021d75
Affan Haq <haqgamer66@gmail.com>
2021-03-15 14:29:12 -0400

    fixed configs search

--- app/Http/Controllers/API/Configs/Actions/GetConfigsForUser.php
+++ app/Http/Controllers/API/Configs/Actions/GetConfigsForUser.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Str;
 
 class GetConfigsForUser extends ConfigsController
 {
-    public function __invoke(Request $request, string $name = null): JsonResponse|AnonymousResourceCollection
+    public function __invoke(Request $request, $name = null): JsonResponse|AnonymousResourceCollection
     {
-        return $name === null
+        return is_null($name)
             ? self::getConfigsForAuthUser($request->user())
             : self::getConfigsForSearchUser($request);
     }
