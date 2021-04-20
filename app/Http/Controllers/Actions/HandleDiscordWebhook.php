97ed37f48b63b872e9417d78f64cf9f0a524d38f
Affan Haq <affan.haq@outlook.com>
2021-04-20 09:43:33 -0400

    fixed HandleDiscordWebhook

--- app/Http/Controllers/Actions/HandleDiscordWebhook.php
+++ app/Http/Controllers/Actions/HandleDiscordWebhook.php
@@ -9,16 +9,6 @@ use Illuminate\Http\Request;
 
 class HandleDiscordWebhook extends Controller
 {
-    private array $roles;
-
-    public function __construct()
-    {
-        $this->roles = [
-            'standard' => intval(config('discord.guild.roles.standard')),
-            'premium' => intval(config('discord.guild.roles.premium')),
-        ];
-    }
-
     public function __invoke(Request $request)
     {
         if (!$request->has('discord_id')) {
@@ -30,27 +20,9 @@ class HandleDiscordWebhook extends Controller
             ->where('discord_id', $request->input('discord_id'))
             ->firstOrFail();
 
-        // user has an active subscription
-        if ($user->subscription !== null) {
-            switch ($user->subscription->plan->id) {
-                case 1:
-                case 3:
-                {
-                    Discord::updateRole($user->discord_id, $this->roles['premium']);
-                    Discord::sendWebhook("Gave <@$user->discord_id> the premium role.");
-                    break;
-                }
-                case 2:
-                {
-                    Discord::updateRole($user->discord_id, $this->roles['standard']);
-                    Discord::sendWebhook("Gave <@$user->discord_id> the standard role.");
-                    break;
-                }
-            }
-        } else {
-            Discord::updateRole($user->discord_id, $this->roles['standard'], true);
-            Discord::updateRole($user->discord_id, $this->roles['premium'], true);
-        }
+        Discord::handleDiscordRoles($user, function (string $discord, string $plan = null) {
+            return "<@$discord> has join the server, so I synchronized their role.";
+        });
 
         return response()->noContent();
     }
