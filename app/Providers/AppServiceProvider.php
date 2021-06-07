883e00d2be7271478b01e5d3d0481d90e62ec5b8
haq <affan.haq@outlook.com>
2021-06-07 01:14:40 -0400

    cleanup

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,27 +7,18 @@ use App\Models\User;
 use App\Observers\InvoiceObserver;
 use App\Observers\UserObserver;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Validation\Rules\Password;
 use Livewire\Component;
 use Stripe\Stripe;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
     public function register()
     {
-        //
     }
 
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
     public function boot()
     {
         JsonResource::withoutWrapping();
@@ -35,10 +26,25 @@ class AppServiceProvider extends ServiceProvider
         User::observe(UserObserver::class);
         Invoice::observe(InvoiceObserver::class);
 
+        // setting the stripe api key
         Stripe::setApiKey(config('services.stripe.secret'));
 
         Component::macro('resetFilePond', fn() => $this->dispatchBrowserEvent('filepond-reset'));
         Component::macro('resetEasyMDE', fn() => $this->dispatchBrowserEvent('easymde-reset'));
         Component::macro('smallNotify', fn($message) => $this->emitSelf('small-notify', $message));
+
+        // blade @admin
+        Blade::if('admin', function () {
+            return auth()->check() && auth()->user()->admin;
+        });
+
+        // default password rules for validation -> Password::defaults()
+        Password::defaults(function () {
+            $rule = Password::min(8);
+
+            return $this->app->isProduction()
+                ? $rule->mixedCase()->uncompromised()
+                : $rule;
+        });
     }
 }
