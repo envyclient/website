bc6f1d24c482d1d0bd20892a5d2935f733b0671c
haq <19542414+haq@users.noreply.github.com>
2021-09-21 17:35:49 -0400

    updated ReferralCodeTest.php

--- tests/Feature/Admin/ReferralCodeTest.php
+++ tests/Feature/Admin/ReferralCodeTest.php
@@ -16,12 +16,13 @@ class ReferralCodeTest extends TestCase
     /** @test */
     public function can_user_not_use_invalid_referral_code()
     {
-        $user = $this->user();
+        $user = self::user();
 
         $this->actingAs($user)
             ->post(route('users.referral-code'), [
                 'referral-code' => 'random_code'
             ])
+            ->assertSessionHasErrors()
             ->assertRedirect();
 
         $this->assertNull($user->referral_code_id);
@@ -30,19 +31,47 @@ class ReferralCodeTest extends TestCase
     /** @test */
     public function can_user_use_valid_referral_code()
     {
-        $user = $this->user();
-        $code = ReferralCode::create([
+        $user = self::user();
+        $referralCode = ReferralCode::factory()->create([
             'user_id' => $user->id,
-            'code' => 'random_code',
         ]);
 
         $this->actingAs($user)
             ->post(route('users.referral-code'), [
-                'referral-code' => $code->code,
+                'referral-code' => $referralCode->code,
             ])
+            ->assertSessionHasNoErrors()
             ->assertRedirect();
 
         $this->assertNotNull($user->referral_code_id);
+    }
+
+    /** @test */
+    public function can_user_not_use_more_than_1_valid_referral_code()
+    {
+        $user = self::user();
+        $referralCodes = ReferralCode::factory()->count(2)->create([
+            'user_id' => $user->id,
+        ]);
+
+        $this->actingAs($user);
+
+        // use first referral code
+        $this->post(route('users.referral-code'), [
+            'referral-code' => $referralCodes[0]->code,
+        ])
+            ->assertSessionHasNoErrors()
+            ->assertRedirect();
+
+        // check if referral code was sued properly
+        $this->assertNotNull($user->referral_code_id);
+
+        // use another referral code
+        $this->post(route('users.referral-code'), [
+            'referral-code' => $referralCodes[1]->code,
+        ])
+            ->assertSessionHas('error')
+            ->assertRedirect();
     }
 
     /** @test */
@@ -54,19 +83,19 @@ class ReferralCodeTest extends TestCase
             ->call('submit')
             ->assertHasErrors(['user', 'code']);
 
-        $this->assertEquals(0, ReferralCode::count());
+        $this->assertDatabaseCount(ReferralCode::class, 0);
     }
 
     /** @test */
     public function can_admin_create_valid_referral_code()
     {
-        $user = $this->admin();
+        $user = self::admin();
 
         Livewire::test(CreateReferralCode::class)
             ->set('user', $user->name)
             ->set('code', Str::random(15))
             ->call('submit');
 
-        $this->assertEquals(1, ReferralCode::count());
+        $this->assertDatabaseCount(ReferralCode::class, 1);
     }
 }
