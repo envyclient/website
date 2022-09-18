f4ef11b434a329b9435cfedd323c3075b543d08c
haq <19542414+haq@users.noreply.github.com>
2022-09-17 21:39:31 -0400

    added upload assets page

---
+++ app/Http/Livewire/Admin/UploadAssets.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Livewire\Admin;
+
+use Livewire\Component;
+use Livewire\WithFileUploads;
+
+class UploadAssets extends Component
+{
+    use WithFileUploads;
+
+    public $assets;
+
+    protected array $rules = [
+        'assets' => ['required', 'file', 'max:5120', 'mimes:zip'],
+    ];
+
+    public function render()
+    {
+        return view('livewire.admin.upload-assets');
+    }
+
+    public function submit()
+    {
+        $this->validate();
+
+        $this->assets->storeAs('', 'assets.zip', 'public');
+
+        $this->done();
+    }
+
+    private function done()
+    {
+        $this->assets = null;
+        $this->resetFilePond();
+        $this->smallNotify('Assets updated.');
+    }
+}
