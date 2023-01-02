33de398ff8da0290bb8b7eee6bc6f28389f2154a
haq <19542414+haq@users.noreply.github.com>
2023-01-01 21:25:13 -0500

    replaced laravel-mix with vite

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,12 +1,16 @@
+import EasyMDE from 'easymde';
+import * as FilePond from 'filepond';
+import Alpine from 'alpinejs';
+
 /**
  * EasyMDE
  */
-window.EasyMDE = require('easymde');
+window.EasyMDE = EasyMDE;
 
 /**
  * FilePond
  */
-window.FilePond = require('filepond');
+window.FilePond = FilePond;
 window.FilePondPluginFileValidateSize = require('filepond-plugin-file-validate-size');
 window.FilePondPluginFileValidateType = require('filepond-plugin-file-validate-type');
 window.FilePondPluginImageValidateSize = require('filepond-plugin-image-validate-size');
@@ -14,7 +18,5 @@ window.FilePondPluginImageValidateSize = require('filepond-plugin-image-validate
 /**
  * Alpine.js
  */
-import Alpine from 'alpinejs'
-
-window.Alpine = Alpine
-Alpine.start()
+window.Alpine = Alpine;
+Alpine.start();
